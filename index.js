dd7ae728a6462b3d5a74cf58107cd3cc99b69b33
JohnnyYours <103307019+JohnnyYours@users.noreply.github.com>
2022-06-10 21:23:58 +0300

    index.js added
    Object destructuring
    Complete the getLatLngElevation function so that it destructures the userLocation object into three variables: lat, lng, and elevation. The default elevation is 0.

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * @param {object} userLocation
+ * @param {number} userLocation.lat
+ * @param {number} userLocation.lng
+ * @param {number} [userLocation.elevation]
+ */
+ const getLatLngElevation = userLocation => {
+	// your code
+}
+
+// Sample usage - do not modify
+const userLocation = {
+  lat: 24.235235,
+  lng: 2.5734,
+};
+
+console.log(getLatLngElevation(userLocation)); 
+// The latitude is 24.235235, the longitude is 2.5734 and the elevation is 0 meters
